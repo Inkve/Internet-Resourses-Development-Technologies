50c0fb60144a257a41f17234a3ca1ef350026bea
Inkve <levin.andrei.2003@gmail.com>
2022-03-29 14:32:56 +0300

    lab4 test

---
+++ #4/JS/structer.js
@@ -0,0 +1,17 @@
+class task{
+    question;
+    answer1;
+    answer2;
+    answer3;
+    answer4;
+    $right_answer;
+
+    constructor(input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans){
+        this.question = input_question;
+        this.answer1 = input_ans1;
+        this.answer2 = input_ans2;
+        this.answer3 = input_ans3;
+        this.answer4 = input_ans4;
+        this.$right_answer = input_rans;
+    };
+}
