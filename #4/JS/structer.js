03d6819812b730a8c79e37d16c9fb7e79d0c5fce
Inkve <levin.andrei.2003@gmail.com>
2022-03-29 19:11:02 +0300

    при нажатии кнопки добавляется блок с ЧТО

--- #4/JS/structer.js
+++ #4/JS/structer.js
@@ -4,7 +4,7 @@ class task{
     answer2;
     answer3;
     answer4;
-    $right_answer;
+    #right_answer;
 
     constructor(input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans){
         this.question = input_question;
@@ -12,15 +12,18 @@ class task{
         this.answer2 = input_ans2;
         this.answer3 = input_ans3;
         this.answer4 = input_ans4;
-        this.$right_answer = input_rans;
+        this.#right_answer = input_rans;
     };
 }
 
 function test_start(){
-    block = document.getElementById("response_field");
+    timer = document.getElementById("timer");
+    time = timer.value;
+    console.log(time.split(":"))
     let a = document.createElement("checkbox");
-    let current_task = task("ЧТО?", "Ответ 1", "Ответ 2", "Ответ 3", "Ответ 4", "VVV" );
+    let current_task = new task("ЧТО?", "Ответ 1", "Ответ 2", "Ответ 3", "Ответ 4", "VVV" );
     creation_checkbox(current_task);
+
 }
 
 
@@ -28,11 +31,15 @@ function test_start(){
 
 
 function creation_checkbox(current){
-    question = document.innerHTMl('$this.question');
-    block.appendChild(question);
+    block = document.getElementById("response_field");
 
-    
+    block.insertAdjacentHTML("afterend", `<input  name=current.answer1 type="checkbox"> </input>` + "</br>")
+    block.insertAdjacentHTML("afterend", `<input id="checkbox" name=(current.answer)${2} type="checkbox"> </input>` + "</br>")
+    block.insertAdjacentHTML("afterend", `<input id="checkbox" name=current.answer${3} type="checkbox"> </input>` + "</br>")
+    block.insertAdjacentHTML("afterend", `<input id="checkbox" name=current.answer${4} type="checkbox"> </input>` + "</br>")
+    block.insertAdjacentHTML("afterend", `<input id="checkbox" name=current.answer${5} type="checkbox"> </input>` + "</br>")
 
+    block.insertAdjacentHTML("afterend", current.question + "</br>")
 }
 
 
