afb0f824213b3f920d581e502aff427fde94a81a
Inkve <levin.andrei.2003@gmail.com>
2022-03-29 16:00:32 +0300

    Special for Inoorik

---
+++ #4/JS/response_field.js
@@ -0,0 +1,13 @@
+block = document.getElementById("response_field");
+let a = document.createElement("checkbox");
+
+let current_task = task("ЧТО?", "Ответ 1", "Ответ 2", "Ответ 3", "Ответ 4", "VVV" );
+
+
+function creation_checkbox(current){
+    
+
+}
+
+
+creation_checkbox(current_task);
--- #4/JS/structer.js
+++ #4/JS/structer.js
@@ -15,3 +15,20 @@ class task{
         this.$right_answer = input_rans;
     };
 }
+
+block = document.getElementById("response_field");
+let a = document.createElement("checkbox");
+
+let current_task = task("ЧТО?", "Ответ 1", "Ответ 2", "Ответ 3", "Ответ 4", "VVV" );
+
+
+function creation_checkbox(current_task){
+    question = document.innerHTMl(this.question);
+    block.append(question);
+
+    
+
+}
+
+
+creation_checkbox(current_task);
