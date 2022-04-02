30f178bc147daa7b9a1c28aed4cb00337874817e
Inkve <levin.andrei.2003@gmail.com>
2022-04-02 16:19:50 +0300

    создание формы с checkbox

--- #4/JS/structer.js
+++ #4/JS/structer.js
@@ -4,7 +4,7 @@ class task{
     answer2;
     answer3;
     answer4;
-    #right_answer;
+    right_answer;
 
     constructor(input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans){
         this.question = input_question;
@@ -12,8 +12,32 @@ class task{
         this.answer2 = input_ans2;
         this.answer3 = input_ans3;
         this.answer4 = input_ans4;
-        this.#right_answer = input_rans;
+        this.right_answer = input_rans;
     };
+};
+
+class task_checkbox extends task{
+    form;
+    div;
+
+    constructor(input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans){
+        super(input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans);
+        this.form = document.createElement('form');
+        this.div = document.createElement('div');
+    }
+
+    checkbox_generate_form(){
+        for (let i = 1; i <= 4; i++){
+            this.form.insertAdjacentHTML("beforeend", `<input type="checkbox" id=answer${i} name=answer${i}>` + this[`answer${i}`] + ' </input> </br>');
+        };
+    };
+
+    checkbox_generate_div(){
+        this.div.insertAdjacentHTML("beforeend", `Это вопрос №${1}` + '</br>');
+        this.div.insertAdjacentHTML("beforeend", 'Название вопроса: ' + this.question + '</br>');
+        this.checkbox_generate_form();
+        this.div.append(this.form)
+    }
 }
 
 
@@ -22,11 +46,57 @@ class task{
 function test_start(){
     let a = document.createElement("checkbox");
     let current_task = new task("ЧТО?", "Ответ 1", "Ответ 2", "Ответ 3", "Ответ 4", "VVV" );
-    creation_checkbox(current_task);
+    //creation_checkbox(current_task);
 
     start_timer("timer");
     
+    let mix_q = form.elements.mix_questions.checked
+    console.log('mix_q: ', mix_q);
+
+    let mix_a = form.elements.mix_answers.checked
+    console.log('mix_a: ', mix_a);
+
+    block = document.getElementById("response_field");
+    div_middle = document.createElement('div');
+    div_middle.insertAdjacentHTML("beforeend", '<input type="time" id="timer_on_test" disabled>' + '</br>');
+    div_middle.insertAdjacentHTML("beforeend", '<button id="prev"> Прошлый </button>' + '<button id="prev"> Следующий </button>');
     
+
+    
+
+    
+    let current1 = new task_checkbox("ЧТО?", "Ответ 1", "Ответ 2", "Ответ 3", "Ответ 4", "VVV" );
+    console.log('current1: ', current1);
+    current1.checkbox_generate_div()
+
+    div_middle.prepend(current1.div)
+    block.replaceWith(div_middle)
+
+    // vvb = document.createElement("fhfh");
+    // vvb.innerHTML = current.question.toString();
+    // block.insertAdjacentHTML("afterend", vvb.value + "</br>")
+    // for (i = 1; i < 5; i++){
+    //     let r1 =  current[`answer${i}`].toString();
+    //     console.log('r1: ', r1);
+    //     block.insertAdjacentHTML("afterend", "<input value=r1 type='checkbox'>"+ r1 + '</input>' + '</br>')
+    // };
+    
+    
+    // <div>
+    //         <div>
+    //             Это вопрос №1 </br>
+    //             Название вопроса
+    //             <form id="form1">
+    //                 <input type="checkbox" id="ans1" name="ans1"> Ответ_1 </input> </br>
+    //                 <input type="checkbox" id="ans2" name="ans2"> Ответ_2 </input> </br>
+    //                 <input type="checkbox" id="ans3" name="ans3"> Ответ_3 </input> </br>
+    //                 <input type="checkbox" id="ans4" name="ans4"> Ответ_4 </input> </br>
+    //             </form>
+    //         </div>
+    //        
+    //     </div>"
+
+
 
 }
 
@@ -59,6 +129,22 @@ function finish_test(){
 };
 
 function initial(){
-    
+    let current = new task("ЧТО?", "Ответ 1", "Ответ 2", "Ответ 3", "Ответ 4", "VVV" );
+    line0 = document.getElementById("response_field");
+    line1 = document.createElement("span");
+    line1.innerHTML = "</br>" + 'Это очень важный опрос' + "</br>";
+    for (i = 1; i < 5; i++){
+        let r1 =  current[`answer${i}`].toString();
+        `line${i++}.innerHTML = "<input value=r1 type='checkbox'>" + r1 + '</input>' + '</br>'`
+        `line${i}.appendChild(line${i++});`
+    };
+
+//     ddffd = 'dhdhhdhd'
+//     console.log('line1: ', line1);
+//     line0.insertAdjacentText("afterend", `${line1}`);
+//     line0.insertAdjacentText("afterend", `${ddffd}`);
 }
 
+//<button onclick="test_start()"> Начать! </button>
+
+//<input type="time" value="00:30:00" min="00:05:00" step="1" max="01:00:00" id="timer" required> 
--- #4/JS/timer.js
+++ #4/JS/timer.js
@@ -4,14 +4,12 @@ class time{
     #time_minutes;
     #time_seconds;
     #seconds;
-    #working;
     constructor(id_timer){
         this.#timer = document.getElementById(id_timer);
         this.#time_hour = Number(this.#timer.value.split(':')[0]);
         this.#time_minutes = Number(this.#timer.value.split(':')[1]);
         this.#time_seconds = Number(this.#timer.value.split(':')[2]);
         this.#seconds = (this.#time_hour * 3600) + (this.#time_minutes * 60) + (this.#time_seconds);
-        this.#working = false;
     };
     timing() {
         if (this.#seconds > 0){
@@ -22,8 +20,9 @@ class time{
             let m1 = (m.toString().length == 2) ? (m) : ('0' + m);
             let s = ((this.#seconds - (h * 60) - (m * 60)) % 60);
             let s1 = (s.toString().length == 2) ? (s) : ('0' + s);
-            timer.value = h1 + ":" + m1 + ":" + s1;
-            console.log('timer.value: ', timer.value);
+            this.#timer.value = h1 + ":" + m1 + ":" + s1;
+            document.getElementById('timer_on_test').value = this.#timer.value;
+            //console.log('timer.value: ', timer.value);
         } else {
             finish_test()
         }
