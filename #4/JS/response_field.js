ca22726b49458b16a03a1e4b9697d0bedd60b0ae
Inkve <levin.andrei.2003@gmail.com>
2022-04-03 23:59:09 +0300

    больше шаблонов

--- #4/JS/response_field.js
+++ #4/JS/response_field.js
@@ -1,17 +1,71 @@
-let current_task1; 
-let current_task2;
-let current_task3;
-let current_task4;
-let current_task5;
-let current_task6;
-let current_task7;
-let current_task8;
+class questions{
+    static all_numer;
+    static all_questions;
+    static questions_div;
+    current_number;
+    current_position;
+    current_name;
+    current_div;
+
+    constructor(){
+        this.all_questions = [];
+        this.questions_div = [];
+        this.all_numer = 8;
+        this.current_number = 0;
+        this.current_position = 0;
+        this.current_name = `current_task${this.current_number + 1}`;
+        this.current_div = 0;
+    };
+
+    generate_checkbox_question(){
+        this.all_questions.push(new task_checkbox(`${this.current_number + 1}`, `ЧТО${this.current_number + 1}?`, `Ответ ${this.current_number + 1}_1`, `Ответ ${this.current_number + 1}_2`, `Ответ ${this.current_number + 1}_3`, `Ответ ${this.current_number + 1}_4`, "VVV", `${this.current_name}`));
+        this.all_questions[this.current_number].checkbox_generate_div();
+        this.questions_div.push(this.all_questions[this.current_number].div)
+        this.current_number++;
+        this.current_name = `current_task${this.current_number + 1}`;
+    }
+
+    generate_radiobutton_question(){
+        this.all_questions.push(new task_radiobutton(`${this.current_number + 1}`, `ЧТО${this.current_number + 1}?`, `Ответ ${this.current_number + 1}_1`, `Ответ ${this.current_number + 1}_2`, `Ответ ${this.current_number + 1}_3`, `Ответ ${this.current_number + 1}_4`, "VVV", `${this.current_name}`));
+        this.all_questions[this.current_number].radiobutton_generate_div();
+        this.questions_div.push(this.all_questions[this.current_number].div)
+        this.current_number++;
+        this.current_name = `current_task${this.current_number + 1}`;
+    }
+
+    current_div_update(){
+        this.current_div = this.questions_div[0];
+    };
+
+    current_div_next(){
+        if (this.current_position < this.current_number - 1){
+            this.current_position++;
+            this.current_div = this.questions_div[this.current_position];
+            this.questions_div[this.current_position - 1].replaceWith(this.current_div);
+        };
+    };
+
+    current_div_prev(){
+        if (this.current_position > 0){
+            this.current_position--;
+            this.current_div = this.questions_div[this.current_position];
+            this.questions_div[this.current_position + 1].replaceWith(this.current_div);
+        };
+    };
+}
+
+let qwerty = new questions;
 
 
 function test_start(){
-    generate_all();
-    
- 
+    qwerty.generate_checkbox_question();
+    qwerty.generate_checkbox_question();
+    qwerty.generate_checkbox_question();
+    qwerty.generate_radiobutton_question();
+    qwerty.generate_radiobutton_question();
+    qwerty.current_div_update();
+    console.log('qwerty: ', qwerty);
+
     start_timer("timer");
     
     let mix_q = start_form.elements.mix_questions.checked
@@ -23,44 +77,25 @@ function test_start(){
     block = document.getElementById("response_field");
     div_middle = document.createElement('div');
     div_middle.insertAdjacentHTML("beforeend", '<input type="time" id="timer_on_test" disabled>' + '</br>');
-    div_middle.insertAdjacentHTML("beforeend", '<button id="prev" onclick="prev_question()"> Прошлый </button>' 
-    + '<button id="next" onclick="next_question()" > Следующий </button>');
+    div_middle.insertAdjacentHTML("beforeend", '<button id="prev" onclick="qwerty.current_div_prev()"> Прошлый </button>' 
+    + '<button id="next" onclick="qwerty.current_div_next()" > Следующий </button>');
     
-    div_middle.prepend(questions_div[0])
+    div_middle.prepend(qwerty.current_div)
+    console.log('qwerty.questions_div[0]: ', qwerty.current_div);
     block.replaceWith(div_middle)
 }
 
 function next_question(){
-    let y = counter;
-    let x = counter + 1;
-    questions_div[y].replaceWith(questions_div[x])
-    counter++;
+    if ((0 <= counter) && (counter <= 3)){
+        let y = counter;
+        let x = counter + 1;
+        questions_div[y].replaceWith(questions_div[x])
+        counter++;
+    };
 }
 
-function prev_question(){
-    let y = counter;
-    let x = counter - 1;
-    questions_div[y].replaceWith(questions_div[x])
-    counter--;
 
-}
 
-let counter = 0;
-let questions_div = new Array;
-function generate_all(){
-    current_task1 = new task_checkbox(`ЧТО1?`, `Ответ 1_1`, `Ответ 1_2`, `Ответ 1_3`, `Ответ 1_4`, "VVV", `current_task1`);
-    current_task2 = new task_radiobutton(`ЧТО2?`, `Ответ 2_1`, `Ответ 2_2`, `Ответ 2_3`, `Ответ 2_4`, "VVV", `current_task2`);
-    current_task3 = new task_checkbox(`ЧТО3?`, `Ответ 3_1`, `Ответ 3_2`, `Ответ 3_3`, `Ответ 3_4`, "VVV", `current_task3`);
-    current_task4 = new task_radiobutton(`ЧТО4?`, `Ответ 4_1`, `Ответ 4_2`, `Ответ 4_3`, `Ответ 4_4`, "VVV", `current_task4`);
-    current_task1.checkbox_generate_div();
-    current_task2.radiobutton_generate_div();
-    current_task3.checkbox_generate_div();
-    current_task4.radiobutton_generate_div();
-    questions_div.push(current_task1.div);
-    questions_div.push(current_task2.div);
-    questions_div.push(current_task3.div);
-    questions_div.push(current_task4.div);
-}
 
 
 
--- #4/JS/structer.js
+++ #4/JS/structer.js
@@ -1,4 +1,5 @@
 class task{
+    number;
     question;
     answer1;
     answer2;
@@ -6,7 +7,8 @@ class task{
     answer4;
     right_answer;
 
-    constructor(input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans){
+    constructor(input_number, input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans){
+        this.number = input_number;
         this.question = input_question;
         this.answer1 = input_ans1;
         this.answer2 = input_ans2;
@@ -26,8 +28,8 @@ class task_checkbox extends task{
     field3;
     field4;
 
-    constructor(input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans, name){
-        super(input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans);
+    constructor(input_number, input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans, name){
+        super(input_number, input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans);
         this.name = name;
         this.fieldset = document.createElement('fieldset');
         this.form = document.createElement('form');
@@ -41,7 +43,7 @@ class task_checkbox extends task{
             checkbox_element.setAttribute("id", `answer${i}`)
             checkbox_element.setAttribute("name", `answer${i}`)
             checkbox_element.setAttribute("value", `answer${i}`)
-            checkbox_element.setAttribute("oninput", `${this.name}.checkbox_save()`)
+            checkbox_element.setAttribute("oninput", `qwerty.all_questions[${this.number - 1}].checkbox_save()`)
             let checkbox_text = document.createElement('span');
             checkbox_text.insertAdjacentHTML("beforeend", `${this[`answer${i}`]}`)
             checkbox_text.insertAdjacentHTML("beforeend", "</br>")
@@ -53,7 +55,7 @@ class task_checkbox extends task{
 
     checkbox_generate_div(){
         this.fieldset.setAttribute("id","test_id")
-        this.div.insertAdjacentHTML("beforeend", `Это вопрос №${1}` + '</br>');
+        this.div.insertAdjacentHTML("beforeend", `Это вопрос №${this.number}` + '</br>');
         this.div.insertAdjacentHTML("beforeend", 'Название вопроса: ' + this.question + '</br>');
         this.checkbox_generate_form();
         this.div.append(this.form)
@@ -62,7 +64,7 @@ class task_checkbox extends task{
     checkbox_save(){
         for (let i = 1; i < 5; i++){
             this[`field${i}`] = document.getElementById(`answer${i}`).checked
-            //console.log(`this[field${i}]: `, this[`field${i}`]);
+            console.log(`this[field${i}]: `, this[`field${i}`]);
         }
     }
 }
@@ -77,8 +79,8 @@ class task_radiobutton extends task{
     field3;
     field4;
 
-    constructor(input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans, name){
-        super(input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans);
+    constructor(input_number, input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans, name){
+        super(input_number, input_question, input_ans1, input_ans2, input_ans3, input_ans4, input_rans);
         this.name = name;
         this.fieldset = document.createElement('fieldset');
         this.form = document.createElement('form');
@@ -92,7 +94,7 @@ class task_radiobutton extends task{
             radiobutton_element.setAttribute("id", `answer${i}`)
             radiobutton_element.setAttribute("name", "radio")
             radiobutton_element.setAttribute("value", `answer${i}`)
-            radiobutton_element.setAttribute("oninput", `${this.name}.radiobutton_save()`)
+            radiobutton_element.setAttribute("oninput", `qwerty.all_questions[${this.number - 1}].radiobutton_save()`)
             let radiobutton_text = document.createElement('span');
             radiobutton_text.insertAdjacentHTML("beforeend", `${this[`answer${i}`]}`)
             radiobutton_text.insertAdjacentHTML("beforeend", "</br>")
@@ -104,7 +106,7 @@ class task_radiobutton extends task{
 
     radiobutton_generate_div(){
         this.fieldset.setAttribute("id","test_id")
-        this.div.insertAdjacentHTML("beforeend", `Это вопрос №${1}` + '</br>');
+        this.div.insertAdjacentHTML("beforeend", `Это вопрос №${this.number}` + '</br>');
         this.div.insertAdjacentHTML("beforeend", 'Название вопроса: ' + this.question + '</br>');
         this.radiobutton_generate_form();
         this.div.append(this.form)
@@ -113,7 +115,7 @@ class task_radiobutton extends task{
     radiobutton_save(){
         for (let i = 1; i < 5; i++){
             this[`field${i}`] = document.getElementById(`answer${i}`).checked
-            //console.log(`this[field${i}]: `, this[`field${i}`]);
+            console.log(`this[field${i}]: `, this[`field${i}`]);
         }
     }
 }
