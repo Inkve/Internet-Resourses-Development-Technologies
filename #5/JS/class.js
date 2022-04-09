067dbd1d498c7852b658e4e3cdd4b02527fd8549
Inkve <levin.andrei.2003@gmail.com>
2022-04-09 20:17:12 +0300

    triangle create

---
+++ #5/JS/class.js
@@ -0,0 +1,49 @@
+class triangle{
+    peak_1_x;
+    peak_1_y;
+    peak_2_x;
+    peak_2_y;
+    peak_3_x;
+    peak_3_y;
+    decrease_x;
+    decrease_y;
+    color;
+
+    constructor(x_1, y_1, x_2, y_2, x_3, y_3, x_decrease, y_decrease, color){
+        this.peak_1_x = x_1;
+        this.peak_1_y = y_1;
+        this.peak_2_x = x_2;
+        this.peak_2_y = y_2;
+        this.peak_3_x = x_3;
+        this.peak_3_y = y_3;
+        this.decrease_x = x_decrease;
+        this.decrease_y = y_decrease;
+        this.color = color;
+    };
+
+    draw(){
+    let canvas = document.getElementById("draw");
+    let field_2d = canvas.getContext('2d');
+    field_2d.beginPath();
+    field_2d.moveTo(this.peak_1_x, this.peak_1_y);
+    field_2d.lineTo(this.peak_2_x, this.peak_2_y);
+    field_2d.lineTo(this.peak_3_x, this.peak_3_y);
+    field_2d.closePath();
+    field_2d.fillStyle = this.color;
+    field_2d.fill();
+    this.udpate_coordinate();
+    };
+
+    udpate_coordinate(){
+        this.peak_1_x = this.peak_1_x + this.decrease_x;
+        this.peak_1_y = this.peak_1_y + this.decrease_y;
+        this.peak_2_x = this.peak_2_x + this.decrease_x;
+        this.peak_2_y = this.peak_2_y + this.decrease_y;
+        this.peak_3_x = this.peak_3_x + this.decrease_x;
+        this.peak_3_y = this.peak_3_y + this.decrease_y;
+    };
+};
+
+
+
+
--- #5/JS/functions.js
+++ #5/JS/functions.js
@@ -1,3 +1,16 @@
+
+$('document').ready(function(){
+    $("#btn").click(function(){
+        test();
+        });
+});
+
+
+
+
+
+
+
 function input_check(){
     let input = document.getElementById("input_number");
     let testing = new RegExp("^([0-9]*)$");
@@ -14,4 +27,29 @@ function input_check(){
     };
     result = Number(result);
     result == 0 ? input.value = "Введите какое-нибудь число" : input.value = result;
+};
+
+let all_figures = [];
+
+
+
+function test(){
+    let one = new triangle(225, 150, 75, 100, 200, 50, 0.1, 0.1, 'pink');
+    let two = new triangle(450, 360, 354, 244, 523, 784, 0.1, -0.1, 'orange');
+    
+    all_figures.push(one);
+    all_figures.push(two);
+    
+    setInterval('draw_all()', 10);
+
+
+};
+
+function draw_all(){
+    let canvas = document.getElementById("draw"); 
+    let field_2d = canvas.getContext("2d"); 
+    field_2d.clearRect(0,0, 1280, 720);
+    for (let i = 0; i < 2; i++){
+        all_figures[i].draw();
+    }
 };
