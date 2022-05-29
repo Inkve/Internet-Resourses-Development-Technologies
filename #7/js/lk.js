2c2639d21b4d4fefaed4fa52b6b2eea24d056300
Inkve <levin.andrei.2003@gmail.com>
2022-05-29 17:51:15 +0300

    1

--- #7/js/lk.js
+++ #7/js/lk.js
@@ -3,6 +3,12 @@ $('document').ready(function(){
     $("#2_main").click(function(){
         window.location.replace('../index.html');
     });
+    $("#2_game").click(function(){
+        window.location.replace('game.html');
+    });
+    $("#2_results").click(function(){
+        window.location.replace('../index.html');
+    });
     $("#right_exit").click(function(){
         window.location.replace('../html/logout.html');
     });
@@ -14,20 +20,26 @@ $('document').ready(function(){
 
 
 function get_info(){
-    let xnr = new XMLHttpRequest();
-    xnr.open("POST", "../php/lk.php");
-    xnr.onload = function(){
-        let data = JSON.parse(xnr.responseText);
-        if (!data["successful"]){
-            window.location.replace('login.html')
-        } else {
-            let login_html = document.createElement("span");
-            login_html.innerHTML = data["login"];
-            document.getElementById("login_lk").replaceWith(login_html);
-            let name_html = document.createElement("span");
-            name_html.innerHTML = data["name"];
-            document.getElementById("name_lk").replaceWith(name_html);
-        };
-    };
-    xnr.send();
+    $.ajax(
+        {
+            url: "../php/lk.php",
+            dataType: "JSON",
+            type: "POST",
+            success: function(data){
+                if (!data["successful"]){
+                    window.location.replace('login.html')
+                } else {
+                    let login_html = document.createElement("span");
+                    login_html.innerHTML = data["login"];
+                    document.getElementById("login_lk").replaceWith(login_html);
+                    let name_html = document.createElement("span");
+                    name_html.innerHTML = data["name"];
+                    document.getElementById("name_lk").replaceWith(name_html);
+                };
+            },
+            error: function(){
+                window.location.replace('login.html');
+            }
+        }
+    );
 };
